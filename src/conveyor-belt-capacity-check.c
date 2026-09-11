8f2f6adf1d16414fd216a8f8791fb9ea04033c61
Marius <s250615@dtu.dk>
2026-09-11 17:14:07 +0200

    Add capacity check for conveyor belt

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -3,7 +3,23 @@
 int MOTOR_CAPACITY = 12;
 
 int main() {
-    int motorCount, totalPackageWeight;
+    int motorCount, totalPackageWeight, maxLoad;
+    printf("How many motors are carrying the packages?\n");
+    scanf("%d", &motorCount);
+    printf("Motors: %d piece\n\n", motorCount);
+    printf("How many motors are carrying the packages?\n");
+    scanf("%d", &totalPackageWeight);
+    printf("Total package weight: %d kg \n\n", totalPackageWeight);
+    maxLoad = motorCount * MOTOR_CAPACITY;
+    printf("Max load: %d kg\n\n\n", maxLoad);
 
-    /// YOUR CODE HERE
+    if(maxLoad>totalPackageWeight){
+        printf("Yes! The conveyor belt can carry the packages.");
+    }
+    else if(maxLoad==totalPackageWeight){
+        printf("The conveyor belt can just about carry the packages.");
+    }
+    else{
+        printf("No. The conveyor belt cannot carry the packages.");
+    }
 }
